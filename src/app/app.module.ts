47fdbb600970ec8dc6deff94e482c65e87dd1fec
Hrant Arakelyan <hrantarakel@gmail.com>
2021-08-16 12:24:35 +0400

    Company web application frontend with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './core/services/employee.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { GlobalTabsComponent } from './components/global-tabs/global-tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    EmployeesComponent,
+    HeaderComponent,
+    ButtonComponent,
+    GlobalTabsComponent,
+    SearchInputComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
